4ab3051b6a7d52f3d5ee5f9b6980a4d12f2f53e4
Ankit Mittal <ankit.mittal@alumni.ubc.ca>
2017-05-27 18:21:41 -0600

    No.7 using Java

---
+++ Q#7.java
@@ -0,0 +1,45 @@
+//Question no. 7
+//Ankit Mittal
+//Solding a problem on Java for the first time!!
+
+
+public class findingtheprime {
+
+    public static void main(String[] args) {
+
+        System.out.println(nthprime(10001));    /*finding the 10001th prime number*/
+
+    }
+
+    /* ths will return the 10001thprime number  */
+    public static long nthprime(long n) {
+
+        int numberOfPrimes = 0;
+        long prime = 1;
+
+        while (numberOfPrimes < n) {
+            prime++;
+            if (isprime(prime)) {
+                numberOfPrimes++;
+            }
+        }
+        return prime;
+    }
+
+    /* returns true only if n happens to be a  prime number */
+
+    public static boolean isprime(long n) {
+
+        if (n < 2) return false;
+
+        else if (n == 2) return true;
+
+        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
+
+            if (n % i == 0)
+
+                return false;
+
+        return true;
+    }
+}
